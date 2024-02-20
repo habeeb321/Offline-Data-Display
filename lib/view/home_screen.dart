0d2bba2e72315a429ba199123dcd711b7c3349a0
habeebu <habeebu@hello24.in>
2024-02-20 15:41:18 +0530

    ui completed

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -16,8 +16,35 @@ class HomeScreen extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      body: const Column(
-        children: [],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.separated(
+                shrinkWrap: true,
+                itemBuilder: (context, index) {
+                  return const ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                    ),
+                    title: Text('hello'),
+                    subtitle: Text('good'),
+                    trailing: Text('Morning'),
+                  );
+                },
+                separatorBuilder: (context, index) =>
+                    const SizedBox(height: 10),
+                itemCount: 10,
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {},
+        label: const Text('Check Connectivity'),
       ),
     );
   }
