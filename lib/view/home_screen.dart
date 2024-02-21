d1aac14df1c2b5b9ea922e3680f12a8a5b6bc915
habeebu <habeebu@hello24.in>
2024-02-21 11:22:30 +0530

    ui modified

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -26,54 +26,64 @@ class HomeScreen extends GetView<CryptoController> {
             GetBuilder<CryptoController>(
               init: CryptoController(),
               builder: (controller) => Expanded(
-                child: controller.loading
+                child: controller.cryptoList.isEmpty
                     ? const Center(
-                        child: CircularProgressIndicator(
-                          backgroundColor: Colors.orange,
-                          strokeWidth: 4,
+                        child: Text(
+                          'No data, Please connect to Internet',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
                         ),
                       )
-                    : ListView.separated(
-                        shrinkWrap: true,
-                        itemBuilder: (context, index) {
-                          return ListTile(
-                            leading: CircleAvatar(
-                              radius: 30,
-                              backgroundColor: Colors.black,
-                              child: CircleAvatar(
-                                radius: 27,
-                                child: Text(
-                                  controller.cryptoList[index].symbol
-                                          .toString()
-                                          .capitalize ??
-                                      'Empty',
+                    : controller.loading
+                        ? const Center(
+                            child: CircularProgressIndicator(
+                              backgroundColor: Colors.orange,
+                              strokeWidth: 4,
+                            ),
+                          )
+                        : ListView.separated(
+                            shrinkWrap: true,
+                            itemBuilder: (context, index) {
+                              return ListTile(
+                                leading: CircleAvatar(
+                                  radius: 30,
+                                  backgroundColor: Colors.black,
+                                  child: CircleAvatar(
+                                    radius: 27,
+                                    child: Text(
+                                      controller.cryptoList[index].symbol
+                                              .toString()
+                                              .capitalize ??
+                                          'Empty',
+                                      style: const TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 15,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                title: Text(
+                                  controller.cryptoList[index].name ?? 'Empty',
+                                  style: const TextStyle(
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                subtitle: Text(
+                                    "Price : ${controller.cryptoList[index].currentPrice ?? 'Empty'}"),
+                                trailing: Text(
+                                  'Market cap rank : ${controller.cryptoList[index].marketCapRank}',
                                   style: const TextStyle(
                                     fontWeight: FontWeight.bold,
                                     fontSize: 15,
                                   ),
                                 ),
-                              ),
-                            ),
-                            title: Text(
-                              controller.cryptoList[index].name ?? 'Empty',
-                              style:
-                                  const TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                            subtitle: Text(
-                                "Price : ${controller.cryptoList[index].currentPrice ?? 'Empty'}"),
-                            trailing: Text(
-                              'Market cap rank : ${controller.cryptoList[index].marketCapRank}',
-                              style: const TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 15,
-                              ),
-                            ),
-                          );
-                        },
-                        separatorBuilder: (context, index) =>
-                            const SizedBox(height: 10),
-                        itemCount: controller.cryptoList.length,
-                      ),
+                              );
+                            },
+                            separatorBuilder: (context, index) =>
+                                const SizedBox(height: 10),
+                            itemCount: controller.cryptoList.length,
+                          ),
               ),
             ),
           ],
